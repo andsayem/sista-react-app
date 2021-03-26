f3580e54af77421a91eaa5cbeac0c4ffd9514da7
AS Sayem <andsayem@gmail.com>
2021-03-26 17:52:44 +0600

    update

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -19,7 +19,7 @@ const  AuthNavigator = () => (
     <Stack.Screen name="Welcome" component={RegisterScreen} options={{ headerShown : false}} />
     <Stack.Screen name="Chats" component={Chats} options={{ headerShown : true}} />
     <Stack.Screen name="Chating" component={Chating} options={{ headerShown : true}} />
-    <Stack.Screen name="Journal_add" component={Journaladd} options={{ headerShown : true}} />
+    <Stack.Screen name="Journal_add" component={Journaladd} options={{ headerShown : false , name : 'Add Journal '}} />
     <Stack.Screen name="PostDetails" component={PostDetails} options={{ headerShown : true}} />
     <Stack.Screen name="Login" component={LoginScreen}  options={{ headerShown : false}} />
     <Stack.Screen name="Register" component={RegisterScreen}  options={{ headerShown : false}} />
