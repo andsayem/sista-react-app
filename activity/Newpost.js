7ad5b68e0d1a704d4731766423a8aaadab285630
AS Sayem <andsayem@gmail.com>
2021-06-01 11:02:15 +0600

    Update

--- activity/Newpost.js
+++ activity/Newpost.js
@@ -9,6 +9,7 @@ import DropDownPicker from 'react-native-dropdown-picker';
 import Loader from '../components/Loader'; 
 import SegmentedControl from '@react-native-community/segmented-control';
 import { launchImageLibrary } from 'react-native-image-picker';
+import api from '../api';
 //import renderIf from './renderIf'
 const Toast = ({ visible, message }) => {
   if (visible) {
@@ -30,7 +31,7 @@ function Newpost({navigation}) {
   const [errortext, setErrortext] = useState(false);
   const [successText, setSuccesstext] = useState(false);   
   const [index, setIndex] = useState(0);
-  const [photo, setPhoto] = useState(null);
+  const [photo, setPhoto] = useState({uri : ""});
   const [getCats, setCats] = useState([]);
 
   useEffect(() => setSuccesstext(false), [successText]);  
@@ -62,8 +63,8 @@ function Newpost({navigation}) {
     console.log('caption Data',dataToSend);
     fetch('http://sista.abdulmazidcse.com/api/post_datas', {
       method: 'POST', 
-      headers: {
-        'Accept': 'application/json',
+      headers: { 
+        Authorization :"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5MmU5Njg1Ny00ZmQ4LTQ0N2ItYjEyZC05MGRjMzBlMTU2MTYiLCJqdGkiOiI2MDQwOGNlMmQ4MzkzM2MxNWI1N2VlOWM0ZGZmYTJlZmQyZjFlMTliM2YzOGIwMDkxNjRhOTkwNThkNTEyNzlhNGE0MGU0NWUwNDRhNzMzMiIsImlhdCI6IjE2MjIxNDAwNjkuMzYxMTMxIiwibmJmIjoiMTYyMjE0MDA2OS4zNjExMzQiLCJleHAiOiIxNjUzNjc2MDY5LjM1ODUyMCIsInN1YiI6IjIiLCJzY29wZXMiOltdfQ.A0w81TgcNg-dRAWmxM1dxYgGsgjwjuMnv2oRPbXsZfzqUqawH7uJl9P9iWKQSYIx2uO8SUxdTE2Ky9zray-fHFKW_TF0MPllEyeg6uh0GWDEveHjz8UV3e1W8TzCj9OYcSDjG40ORuKNZrPK2WgrPOzjjyr0w-vhmn4tyBL1XEHlW4gRxRYBjdXBay5LBjSHF_k_7cv_DagK4bscjWYTC5sJpjjYOVIKBj1vyQo6z13s6tAK6DPS899bM89E7AqW_JdUuwI95R6UwVnl1OqFvc0j2DArrHl1XLWa0-iRMseM1k4zibQA10-mzBftuJ-ivfH0zxRQAMg_9U4wIlSkFtNpGF3r93pmdvgvhhoShyEwwvsG8UGr_a3hIq23v2xXBID8flW7239AI__Ss8eDOPoq0A-_B3FFDu2TPVnej7cYl1PS29zi6EtXMVVM2o2hSHKCcY2m5OornonklaUZLrJwWShNG2SchxuLqhAVZsZFoHUjqH6R_451Keke2wyZXNkfprb7MIqiogBkkUWUQtyl5O9qducahbnWwY6CkSNNiLo0rxPhxYYsgQ_oUWUYiYZNhm8FUOIFWMaGQOH56dJVrKAZ-UIU-0Bqt6Zi4FuE6uxjGMHFzbRFT_CYlmC3xE3-F8y_WswE0nqqqYkEe1qnLOGG400I5XQ05mtWXik",
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(dataToSend) 
@@ -107,16 +108,24 @@ function Newpost({navigation}) {
     });
   };
   const getCategories = async => {
-    fetch('http://sista.abdulmazidcse.com/api/post_categories', {
-      method: 'GET',  
-      })
-      .then((response) => response.json())
-      .then((responseJson) => { 
-        setCats(responseJson.data);
+    api.getData('post_categories')
+    .then((res)=>{  
+        setCats( res.data.data);
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-        console.log('getCatss', getCats);
-        console.log('newpost_res',responseJson.data);  
-      })
+    // fetch('http://sista.abdulmazidcse.com/api/post_categories', {
+    //   method: 'GET',  
+    //   })
+    //   .then((response) => response.json())
+    //   .then((responseJson) => { 
+    //     setCats(responseJson.data);
+
+    //     console.log('getCatss', getCats);
+    //     console.log('newpost_res',responseJson.data);  
+    //   })
   };
   ChildViewEliment=()=>{
     if(index == 0){
@@ -193,16 +202,16 @@ function Newpost({navigation}) {
           /> 
           <Toast style={Styles.errorTextStyle} visible={errortext} message={errortext.message} ref={(ref) => Toast.setRef(ref)}/>
           <Toast visible={successText} message ={successText.message} />
-              {photo.uri(     
+                    
               <ListItem>
                 <ListItem.Content > 
                 <Image 
-                  source={{ uri: photo ?  photo.uri : '' }}
-                  style={{ width: 300, height: 300 }}
+                  source={{ uri: photo.uri ?  photo.uri : '' }}
+                  style={{  width: '100%', height: photo.uri  ? 300 : 0 }}
                 />
                 </ListItem.Content> 
               </ListItem>  
-          )}
+           
           <ListItem > 
               <ListItem.Content  > 
                   <ListItem.Content >
