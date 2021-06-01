09bc7730e8f45d9054299581954b2d6167d05a7c
abdulmazidcse <52539549+abdulmazidcse@users.noreply.github.com>
2021-06-02 00:24:52 +0600

    post like

--- activity/home-bk.js
+++ activity/home-bk.js
@@ -1,44 +1,58 @@
 
 
 import React, { Component, useEffect, useState} from "react";
-import { View, FlatList ,Text, Image, Alert , Button ,TextInput, TouchableOpacity, ToastAndroid, StyleSheet } from "react-native";
+import { View, FlatList ,Text, Image, Button , ImageBackground ,TextInput, TouchableOpacity, ToastAndroid, StyleSheet } from "react-native";
 //import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { ScrollView  } from "react-native-gesture-handler";
 import { ListItem, Avatar, colors , Icon , Header } from 'react-native-elements'; 
 import Styles from "../styles"; 
-import AsyncStorage from '@react-native-community/async-storage';   
+import AsyncStorage from '@react-native-community/async-storage';  
+import axios from 'axios';
 import api from '../api';
 const STORAGE_KEY = 'save_user';
 const TOKEN = 'token'; 
 
-
-const Toast = ({ visible, message }) => {
-  if (visible) {
-    ToastAndroid.showWithGravityAndOffset(
-      message,
-      ToastAndroid.LONG,
-      ToastAndroid.BOTTOM,
-      25,
-      50
-    );
-    return null;
-  }
-  return null;
-};
-
-const likeSubmitButton = (postid) => {    
-  console.log('tes===========================' ,postid);   
-  api.getData('postlike/'+ postid)
+const likeSubmitButton = (postid) => {   
+  console.log('tes===========================' ,postid); 
+  // console.log('postid', postid);
+  // var dataToSend = { 
+  //   user_id: 40,
+  //   post_id: postid,
+  //   comm_id: 0, 
+  // }; 
+  let post_id  = 11 ; 
+  api.getData('postlike/'+ post_id)
   .then((res)=>{
-    alert(res.data.message);
-      console.log(res.data);  
+      setItems( res.data.data);  
   })
   .catch((error) => {
       console.log(error)
-  }) 
+  })
+
+
+  // fetch('http://sista.abdulmazidcse.com/api/all_likes', {
+  //   method: 'POST', 
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify(dataToSend) 
+  //   })
+  //   .then((response) => response.json())
+  //   .then((responseJson) => { 
+  //     console.log('newpost_res',responseJson);  
+  //   })
+  //   .catch((error) => { 
+  //     console.error(error);
+  //   });
 }; 
-const Item = ({ ItemData, Shortcaption }) => (
-  <View  style={{   backgroundColor: '#fff' , height: 310,  width: '100%', borderRadius: 15,   padding: 10,  marginBottom :10  }} >
+
+// MyPosts =[
+//   { caption : 'Lorem impsum dolor sit amet, consectetuer adipscing elit,  consectetuer adipscing elit,' ,  created_at : '2021-04-30 08:46:08' }, 
+// ];
+ChildView=({ ItemData ,Shortcaption})=>{
+  return(
+    <View  style={{   backgroundColor: '#fff' , height: 310,  width: '100%', borderRadius: 15,   padding: 10,  marginBottom :10  }} >
     <ListItem style={{  backgroundColor: "#FEFEFE", width: '100%',    }}>
       <Avatar rounded   size="medium" source={require('../img/images/user_3.jpg')} />
       <ListItem.Content >
@@ -53,14 +67,13 @@ const Item = ({ ItemData, Shortcaption }) => (
      {Shortcaption} 
     </Text> 
     <Image onPress={() => navigation.navigate('PostDetails') } source={ItemData ? {uri: ItemData.file } : null}  style={{ width: '100%', borderRadius: 10, height: 130 }}   />
-   
-    <View  horizontal   showsHorizontalScrollIndicator={false} style={{ marginRight: -40, marginTop: 10 }}  > 
+    <ScrollView  horizontal   showsHorizontalScrollIndicator={false} style={{ marginRight: -40, marginTop: 10 }}  > 
       
       <TouchableOpacity
        
           activeOpacity={0.5} >
           <View style={{   height: 66,  width: 80, }}  >
-            <Text style={{ color : '#a21919'}}  onPress={() => likeSubmitButton(ItemData.id)} >Like {ItemData.like}  {ItemData.id}  </Text>
+            <Text style={{ color : '#a21919'}} onClick={likeSubmitButton.bind(ItemData.id)}> Like {ItemData.like}  {ItemData.id}  </Text>
           </View>
       </TouchableOpacity>  
       <View  style={{   height: 66, width: 120,    }} >
@@ -69,17 +82,78 @@ const Item = ({ ItemData, Shortcaption }) => (
       <View style={{  height: 66,  width: 100,  }}  >
         <Text style={{ color : '#1c81b0'}} > Share {ItemData.share} </Text>
       </View>
-    </View>
+    </ScrollView>
   </View> 
-);
+  ) 
+}
+const Toast = ({ visible, message }) => {
+  if (visible) {
+    ToastAndroid.showWithGravityAndOffset(
+      message,
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+      25,
+      50
+    );
+    return null;
+  }
+  return null;
+};
  
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 function Home ({navigation}){ 
-  const [PostItems, setItems] = useState([]);  
+  const [PostItems, setItems] = useState([]); 
+      // axios.get('https://sista.abdulmazidcse.com/api/post_datas',{
+      //     headers: {
+      //         "Content-Type": "application/json", 
+      //         Authorization :"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5MmU5Njg1Ny00ZmQ4LTQ0N2ItYjEyZC05MGRjMzBlMTU2MTYiLCJqdGkiOiI2MDQwOGNlMmQ4MzkzM2MxNWI1N2VlOWM0ZGZmYTJlZmQyZjFlMTliM2YzOGIwMDkxNjRhOTkwNThkNTEyNzlhNGE0MGU0NWUwNDRhNzMzMiIsImlhdCI6IjE2MjIxNDAwNjkuMzYxMTMxIiwibmJmIjoiMTYyMjE0MDA2OS4zNjExMzQiLCJleHAiOiIxNjUzNjc2MDY5LjM1ODUyMCIsInN1YiI6IjIiLCJzY29wZXMiOltdfQ.A0w81TgcNg-dRAWmxM1dxYgGsgjwjuMnv2oRPbXsZfzqUqawH7uJl9P9iWKQSYIx2uO8SUxdTE2Ky9zray-fHFKW_TF0MPllEyeg6uh0GWDEveHjz8UV3e1W8TzCj9OYcSDjG40ORuKNZrPK2WgrPOzjjyr0w-vhmn4tyBL1XEHlW4gRxRYBjdXBay5LBjSHF_k_7cv_DagK4bscjWYTC5sJpjjYOVIKBj1vyQo6z13s6tAK6DPS899bM89E7AqW_JdUuwI95R6UwVnl1OqFvc0j2DArrHl1XLWa0-iRMseM1k4zibQA10-mzBftuJ-ivfH0zxRQAMg_9U4wIlSkFtNpGF3r93pmdvgvhhoShyEwwvsG8UGr_a3hIq23v2xXBID8flW7239AI__Ss8eDOPoq0A-_B3FFDu2TPVnej7cYl1PS29zi6EtXMVVM2o2hSHKCcY2m5OornonklaUZLrJwWShNG2SchxuLqhAVZsZFoHUjqH6R_451Keke2wyZXNkfprb7MIqiogBkkUWUQtyl5O9qducahbnWwY6CkSNNiLo0rxPhxYYsgQ_oUWUYiYZNhm8FUOIFWMaGQOH56dJVrKAZ-UIU-0Bqt6Zi4FuE6uxjGMHFzbRFT_CYlmC3xE3-F8y_WswE0nqqqYkEe1qnLOGG400I5XQ05mtWXik"
+      //     },
+      // })
+      //   .then((res) => { 
+      //     setItems( res.data.data); 
+      // }).catch(function (error) {
+      //   console.log(error);
+      // }); 
+      api.getData('post_datas')
+      .then((res)=>{
+          setItems( res.data.data);  
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+
+  //let respJsone  =   resp.json(); 
   const [users, setUser] = useState('');
   const [successtext, setSuccesstext] = useState(false);
   const [errortext, setErrortext] = useState(false);
   const [getCats, setCats] = useState([]);
-   
+  const readData = async () => {
+    // try {
+    //   const userInfo =  AsyncStorage.getItem(STORAGE_KEY);
+      
+    //   //console.log('HomePage==========',jsonuser.access_token);
+    //   if (userInfo !== null) {
+    //     setUser(jsonuser)
+    //   }else{
+    //     navigation.replace('Login')
+    //   }
+    // } catch (e) {
+    //   setErrortext({ message: 'Failed to save the data to the storage' }); 
+    // }
+  } 
+  // const clearStorage = async () => {
+  //   // try {
+  //   //   await AsyncStorage.clear()
+  //   //   navigation.replace('Login')
+  //   //   setSuccesstext({ message:'Storage successfully cleared!' }); 
+  //   // } catch (e) {
+  //   //   setErrortext({ message: 'Failed to save the data to the storage' });  
+  //   // }
+  // }
   const getCategories = async => {
     api.getData('post_categories')
     .then((res)=>{
@@ -87,48 +161,37 @@ function Home ({navigation}){
     })
     .catch((error) => {
         console.log(error)
-    }) 
-  };  
+    })
 
-  const getPosts = () =>{
-    api.getData('post_datas')
-      .then((res)=>{
-          setItems( res.data.data);  
-          console.log('posts=====',res.data.data)
-      })
-      .catch((error) => {
-          console.log(error)
-      }) 
-  }
+    // fetch('http://sista.abdulmazidcse.com/api/post_categories', {
+    //   method: 'GET',  
+    //   })
+    //   .then((response) => response.json())
+    //   .then((responseJson) => { 
+    //     setCats(responseJson.data); 
+    //   })
+  };
  
+  useEffect(() => {
+    readData();
+  },[]) 
   useEffect(() => setSuccesstext(false), [successtext]); 
   useEffect(() => setErrortext(false), [errortext]);
-  useEffect(() => getCategories(false),[getCats]); 
-  useEffect(() => getPosts(false),[PostItems]); 
- 
-
-  const renderItem = ({ item }) => { 
-    return (
-      <Item
-        ItemData={item}  
-        keyExtractor={(item) => item.id} 
-      />
-    );
-  };
+  useEffect(() => getCategories(false)); 
 
   return (
-      <ScrollView>
+      <ScrollView >
         <Header
-            style={{ backgroundColor : 'red'}}
+        style={{ backgroundColor : 'red'}}
               leftComponent={<Icon color={colors.white} size={30} name='menu' onPress={() => navigation.toggleDrawer()} />}
               centerComponent={{ text: 'Inspire me', style: { color: '#fff' , fontSize : 20 } }}
               rightComponent={{ icon: 'notifications', color: '#fff' }}
-                /> 
+            /> 
         <View style={{ paddingHorizontal: 10 , backgroundColor: '#fff' , paddingBottom : 0}}>
         <Toast visible={errortext} message={errortext.message} />
-        <Toast visible={successtext} message={successtext.message} /> 
+        <Toast visible={successtext} message={successtext.message} />
           <Text style={Styles.box_title} >
-            Events Alert
+            Events
           </Text> 
           <ScrollView
             horizontal
@@ -562,11 +625,13 @@ function Home ({navigation}){
                     </ListItem.Content>
             </ListItem>
         </ScrollView>
+            
+         
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
           style={{ marginRight: -30}}
-          >
+        >
             <ListItem style={{ padding : 0 , margin : 0}} > 
               <ListItem.Content style={{ padding : 0 , margin : 0 , marginRight : -10  , marginLeft  : -10}} > 
               <TouchableOpacity              
@@ -610,11 +675,9 @@ function Home ({navigation}){
           </ScrollView>
         </View>
         <View style={{ marginHorizontal :10 , borderRadius: 10,   paddingHorizontal: 8 , paddingBottom : 15 ,   marginTop : 10}} > 
-        <FlatList
-          data={PostItems}
-          renderItem={renderItem}
-          keyExtractor={(PostItems) => PostItems.id} 
-        />
+          {/* Item start  */}
+              <FlatList data={PostItems} renderItem={({item})=><ChildView Shortcaption={item.short_caption} ItemData={item} />} />
+          {/*Item end*/} 
         </View>
       </ScrollView>
   );
