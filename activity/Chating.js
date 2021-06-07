98c84b6b130a0ac154602880de1fd97a0178d6b0
abdulmazidcse <52539549+abdulmazidcse@users.noreply.github.com>
2021-06-07 21:43:04 +0600

    chating

--- activity/Chating.js
+++ activity/Chating.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from "react";
-import { View, Text, Image, Button , ImageBackground ,TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, Image, Button , ToastAndroid ,TextInput, TouchableOpacity, StyleSheet } from "react-native";
 //import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { ScrollView  } from "react-native-gesture-handler";
 import { ListItem, Avatar  } from 'react-native-elements'; 
@@ -7,26 +7,69 @@ import BottomSheet from 'react-native-simple-bottom-sheet';
 import { Icon } from 'react-native-elements'
 import Styles from "../styles";
 import api from '../api';
+import Textarea from 'react-native-textarea';
+import post_api from '../post_api';
 function Chating({navigation, route}) {
-  const { item } = route.params;
+  const { sender_id } = route.params;
   const [senderId, setSenderId] = useState(null);
   const [receiverId, setReceiverId] = useState(null);
-  const [getUser, setUser] = useState([]); 
-  //setSenderId(item);
-  //setSenderId(route.params);  
+  const [message, setMessage] = useState(false);
+  const [getUser, setUser] = useState([]);  
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState(false);
+  const [successText, setSuccesstext] = useState(false);   
+  const [index, setIndex] = useState(0);
+  const Toast = ({ visible, message }) => {
+    if (visible) {
+      ToastAndroid.showWithGravityAndOffset(
+        message,
+        ToastAndroid.LONG,
+        ToastAndroid.TOP,
+        25,
+        50
+      );
+      return null;
+    }
+    return null;
+  }; 
   const user = async => { 
-    api.getData('users/'+route.params)
+    api.getData('users/'+sender_id)
       .then((res)=>{
         setUser( res.data.data);  
-          console.log('users',res.data.data)
+        setSenderId(sender_id); 
       })
       .catch((error) => {
           //console.log(error)
       }) 
   }
-  useEffect(() => user(),[getUser]);  
+  const handleSubmitButton = () => {    
+    setErrortext(false);
+    if (!message) { 
+      setErrortext({message : 'Please fill caption'});  
+      return;
+    }else{
+      setSuccesstext(false);
+    }
+    setLoading(true); 
+    var dataToSend = { 
+      receiverId: 1,
+      senderId: senderId,
+      caption: message
+    }; 
+    post_api.postData('users/'+sender_id,dataToSend)
+      .then((res)=>{
+        setUser( res.data.data);  
+        setSenderId(sender_id); 
+      })
+      .catch((error) => {
+          //console.log(error)
+      }) 
+  }
+  useEffect(() => user(false),[getUser]);  
+  //useEffect(() => errortext(false),[setSuccesstext(false)]);   
     return (
       <ScrollView > 
+        
         <View  style={{  paddingTop : 0  ,   backgroundColor: "#efefef",  }}  > 
             <View >
               <ListItem style={{
@@ -104,10 +147,82 @@ function Chating({navigation, route}) {
               <Text style={{ backgroundColor : '#E4E4E4' ,  borderRadius: 7, padding :5 , textAlign : 'left' }}> A wonderful Society has taken  possession of my entire soul,  like these sweet mornings of from bottom of my heart  </Text>   
             </ListItem.Content>
           </ListItem> 
-         
+          
+          <View style={styles.textAreaContainer} >
+                
+                <Textarea
+                  onChangeText={(message) => setMessage(message)} 
+                  value={message}
+                  blurOnSubmit={true}
+                  containerStyle={styles.textareaContainer}
+                  style={styles.textarea}
+                  maxLength={1000}
+                  placeholder={'Type something...'}
+                  placeholderTextColor="grey" 
+                  returnKeyType="next"
+                  multiline={true}
+                  underlineColorAndroid="transparent"
+                  underlineColorAndroid={'transparent'}
+                />
+                </View>
+              <Toast style={Styles.errorTextStyle} visible={errortext} message={errortext.message} ref={(ref) => Toast.setRef(ref)}/>
+              <Toast visible={successText} message ={successText.message} />
+              <TouchableOpacity
+             onPress={handleSubmitButton} 
+              style={Styles.journalBtn}
+              activeOpacity={0.5} >
+              <Text style={Styles.journalText}               
+              >Submit</Text>
+            </TouchableOpacity>
       </ScrollView>
       
     );
 }
 
+const styles = StyleSheet.create({
+  textAreaContainer: {
+    borderColor:  '#efefef',
+    borderWidth: 1,  
+  },
+  textArea: {
+    height: 150, 
+    
+  } ,
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textareaContainer: {
+    height: 180,
+    padding: 5,
+    backgroundColor: '#F5FCFF',
+  },
+  textarea: {
+    textAlignVertical: 'top',  // hack android
+    height: 170,
+    fontSize: 14,
+    color: '#333',
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
 export default Chating;
--- post_api.js
+++ post_api.js
@@ -2,16 +2,17 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 const STORAGE_KEY = 'save_user';
 const TOKEN = 'token'; 
-
-
-export default { getData: (path) =>
+ 
+export default { postData: (path, dataToSend) =>
     axios({
-        'method':'GET',
+        'method':'POST',
         'url':'https://sista.abdulmazidcse.com/api/'+path,
         'headers': {
             "Content-Type": "application/json", 
             Authorization :"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5MmU5Njg1Ny00ZmQ4LTQ0N2ItYjEyZC05MGRjMzBlMTU2MTYiLCJqdGkiOiI2MDQwOGNlMmQ4MzkzM2MxNWI1N2VlOWM0ZGZmYTJlZmQyZjFlMTliM2YzOGIwMDkxNjRhOTkwNThkNTEyNzlhNGE0MGU0NWUwNDRhNzMzMiIsImlhdCI6IjE2MjIxNDAwNjkuMzYxMTMxIiwibmJmIjoiMTYyMjE0MDA2OS4zNjExMzQiLCJleHAiOiIxNjUzNjc2MDY5LjM1ODUyMCIsInN1YiI6IjIiLCJzY29wZXMiOltdfQ.A0w81TgcNg-dRAWmxM1dxYgGsgjwjuMnv2oRPbXsZfzqUqawH7uJl9P9iWKQSYIx2uO8SUxdTE2Ky9zray-fHFKW_TF0MPllEyeg6uh0GWDEveHjz8UV3e1W8TzCj9OYcSDjG40ORuKNZrPK2WgrPOzjjyr0w-vhmn4tyBL1XEHlW4gRxRYBjdXBay5LBjSHF_k_7cv_DagK4bscjWYTC5sJpjjYOVIKBj1vyQo6z13s6tAK6DPS899bM89E7AqW_JdUuwI95R6UwVnl1OqFvc0j2DArrHl1XLWa0-iRMseM1k4zibQA10-mzBftuJ-ivfH0zxRQAMg_9U4wIlSkFtNpGF3r93pmdvgvhhoShyEwwvsG8UGr_a3hIq23v2xXBID8flW7239AI__Ss8eDOPoq0A-_B3FFDu2TPVnej7cYl1PS29zi6EtXMVVM2o2hSHKCcY2m5OornonklaUZLrJwWShNG2SchxuLqhAVZsZFoHUjqH6R_451Keke2wyZXNkfprb7MIqiogBkkUWUQtyl5O9qducahbnWwY6CkSNNiLo0rxPhxYYsgQ_oUWUYiYZNhm8FUOIFWMaGQOH56dJVrKAZ-UIU-0Bqt6Zi4FuE6uxjGMHFzbRFT_CYlmC3xE3-F8y_WswE0nqqqYkEe1qnLOGG400I5XQ05mtWXik"
              //Authorization :"Bearer "+ AsyncStorage.getItem(TOKEN)
-        }
+        },
+        body: JSON.stringify(dataToSend) 
     }) 
-}; 
+}
+   
