0ffc25d35284911153aa1da85e851d96134295d5
AS Sayem <andsayem@gmail.com>
2022-03-27 09:49:58 +0600

    Support Page Update UI

--- activity/support/Support.js
+++ activity/support/Support.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
-import { View, Text, Image, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, Image, FlatList, SafeAreaView, StyleSheet } from "react-native";
 import { colors, Icon, Header } from 'react-native-elements';
+import { RadioButton } from 'react-native-paper';
+import api from '../../api';
+import Textarea from 'react-native-textarea';
 //function About({navigation}) {
 class Support extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false,
+      items: []
+    };
+    this.getSupport();
+    //this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
+  }
+  getSupport = async => {
+    api.getData('support_types')
+      .then((res) => {
+        this.setState({ items: res.data.data });
+      })
+      .catch((error) => {
+      })
+  };
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView styles={{ backgroundColor : '#ffffff'}}>
         <Header
           leftComponent={<Icon color={colors.black} size={30} name='menu'
             onPress={() => this.props.navigation.toggleDrawer()} ></Icon>}
@@ -16,7 +37,7 @@ class Support extends React.Component {
             backgroundColor: '#E4E4E4'
           }}
         />
-        <View>
+        <View style={{ backgroundColor: '#FFFFFF' }}>
           <View style={{ textAlign: 'center', paddingTop: 30 }}>
             <Image style={{ alignSelf: 'center' }} source={require('../../img/bg/support.png')} />
           </View>
@@ -25,11 +46,54 @@ class Support extends React.Component {
         </View>
         <View
           style={{
-            paddingStart : 20, 
+            backgroundColor: '#FFFFFF',
+            paddingStart: 20,
             borderBottomColor: '#CEC7C7',
-            borderBottomWidth: 1 
+            borderBottomWidth: 1
           }}
         />
+        <View style={{ backgroundColor: '#FFFFFF' }}>
+          <FlatList
+            style={{ margin: 10 }}
+            data={this.state.items}
+            renderItem={({ item, index }) => {
+              return (
+                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                  <View style={{ textAlign: 'right' }}>
+                    <RadioButton
+                      value="first"
+                    //status={checked === 'first' ? 'checked' : 'unchecked'}
+                    // onPress={() => setChecked('first')}
+                    />
+                  </View>
+                  <View style={{ flex: 4 }}>
+                    <Text style={{ color: '#000000' }}>{item.title}</Text>
+                  </View>
+                </View>
+              )
+            }
+            }
+            ListFooterComponent={this.renderFooter}
+            onEndReachedThreshold={200}
+            refreshing={this.state.refreshing}
+            onRefresh={this.handleOnRefresh}
+          />
+          <View
+           style={{ paddingStart: 25, paddingEnd: 25  , backgroundColor : '#ffffff'}}>
+            <Textarea
+              //onChangeText={(post_caption) => setCaption(post_caption)}
+              // value={post_caption}
+              blurOnSubmit={true}
+              containerStyle={styles.textareaContainerBg}
+              backgroundColor="rgba(0,0,0,0)"
+              maxLength={1000}
+              placeholder={'Submit your review'}
+              returnKeyType="next"
+              multiline={true}
+              underlineColorAndroid={'transparent'}
+            />
+          </View>
+        </View>
       </SafeAreaView>
     );
   }
